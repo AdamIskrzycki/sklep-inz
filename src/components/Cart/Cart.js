b1c194064c910871ccd478eff9e47fd875b72614
adam <aiskrzycki01@gmail.com>
2019-09-15 14:26:05 +0200

    displaying info in cart fixed

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,13 +3,23 @@ import "./Cart.css";
 
 const Cart = (props) => {
     
-    const totalPrice = props.products.reduce((acc, product) => acc + product.discountedPrice, 0)
+    const totalPrice = props.products.reduce((acc, product) => acc + product.discountedPrice, 0);
+    const productInfo = props.products.map(product => {
+        return (
+        <>
+            <span>{product.name + " "}</span>
+            <span>{product.discountedPrice}</span>
+            <br/>
+        </>
+         )
+    })
 
     return (
         <div className="Cart">
             <p style={{textDecoration: 'underline'}}>CART</p>
             <p>{props.products ? props.products.length : "no items!"}</p>
             <p>{totalPrice}</p>
+            <p>{productInfo}</p>
         </div>
     );
 }
