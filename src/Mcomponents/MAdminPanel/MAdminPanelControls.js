ffc93a86254ff032144425af1fc3773387c641aa
adam <aiskrzycki01@gmail.com>
2020-05-09 20:40:10 +0200

    Admin Panel updated

--- src/Mcomponents/MAdminPanel/MAdminPanelControls.js
+++ src/Mcomponents/MAdminPanel/MAdminPanelControls.js
@@ -50,30 +50,23 @@ class MAdminPanelControls extends Component {
 
         const { classes } = this.props
 
-        const inputPropsName = {
-            //value: this.state.name,
-            //onChange: this.onInputChange
+
+        const inputProps = {
+            step: '0.01',
+            min: '0'    
         }
 
-        const inputPropsPrice = {
-            value: this.state.price,
-            onChange: this.onInputChange
-        }
 
-        const inputPropsDiscountedPrice = {
-            value: this.state.discountedPrice,
-            onChange: this.onInputChange
-        }
 
         return(
             <React.Fragment>
                 <Container maxWidth='sm' className={classes.container}>
                     <Typography component="h1" variant="h4" align="center" color="textPrimary" gutterBottom>Welcome to the Admin Panel</Typography>
                     <Typography variant="h5" align="center" color="textSecondary" paragraphvariant="h5" paragraph>Please specify the properties below in order to add a new product to the stock.</Typography>
-                    <TextField inputProps={inputPropsName}  id='focus' label="Name" variant="outlined" className={classes.textField} autoFocus/>
-                    <TextField inputProps={inputPropsPrice} type='number' label="Price" variant="outlined" className={classes.textField}/>
-                    <TextField inputProps={inputPropsDiscountedPrice} type='number' label="Discounted Price (optional)" variant="outlined" className={classes.textField}/>
-                    <Button variant='contained' color='primary' className={classes.addButton} onClick={this.clearInputs}>ADD</Button>
+                    <TextField value={this.state.name} onChange={this.onInputChange} name='name' id='focus' label="Name" variant="outlined" className={classes.textField} autoFocus/>
+                    <TextField value={this.state.price} inputProps={inputProps} onChange={this.onInputChange} name='price' type='number' label="Price" variant="outlined" className={classes.textField}/>
+                    <TextField value={this.state.discountedPrice} inputProps={inputProps} onChange={this.onInputChange} name='discountedPrice' type='number' label="Discounted Price" variant="outlined" className={classes.textField}/>
+                    <Button disabled={this.state.isInputEmpty} variant='contained' color='primary' className={classes.addButton} onClick={this.clearInputs}>ADD</Button>
                 </Container>
 
             </React.Fragment>
