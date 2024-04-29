9b245890ee68b55f1a029b1f48308d3f167a1dba
AdamIskrzycki <aiskrzycki01@gmail.com>
2024-04-29 20:08:13 +0200

    added env file

--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,14 @@ import "firebase/firestore";
 import "firebase/auth";
 import "firebase/storage"
 
+const apiKey = process.env.API_KEY
+
 var firebaseConfig = {
-  apiKey: "AIzaSyDFks6hXNYoeRYisY1o0DP4jm4ikuhb27g",
+  apiKey: apiKey, 
   authDomain: "sklep-inz.firebaseapp.com",
   databaseURL: "https://sklep-inz.firebaseio.com",
   projectId: "sklep-inz",
   storageBucket: "sklep-inz.appspot.com",
-  //messagingSenderId: "992445087980",
-  //appId: "1:992445087980:web:7ed036f056fa31fbe625c9",
-  //measurementId: "G-Z6RCJK8JEP",
 };
 
 firebase.initializeApp(firebaseConfig);
